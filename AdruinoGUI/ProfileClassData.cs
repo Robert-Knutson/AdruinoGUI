c23491272de40a6e239595ae632c36e913297545
RobDog7 <63023502+RobDog7@users.noreply.github.com>
2023-03-28 19:10:57 -0600

    Added option to save data changed in data table as CSV, and added what will be the motor controller tab

---
+++ AdruinoGUI/ProfileClassData.cs
@@ -0,0 +1,22 @@
+﻿using CsvHelper.Configuration.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdruinoGUI
+{
+    public class ProfileClassData
+    {
+        [Index(0)]
+        public int Time { get; set; }
+
+        [Index(1)]
+        public int LeftMotor { get; set; }
+
+        [Index(2)]
+        public int RightMotor { get; set; }
+
+    }
+}
