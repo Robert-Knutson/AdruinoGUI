120bac211fb64ec761735cc5859ec87a000e33e4
RobDog7 <63023502+RobDog7@users.noreply.github.com>
2023-03-31 13:23:30 -0600

    New Update! Massive overhaul on the serial comunication sruff. All serial is now asynchrinous!!! We can read and write to the serial port. I also added in hte motor speed canculator and the logic to write the correct speeds to the serial port at the correct time, as governed by the timing values in the perturbation profile. This is a bug deal. Are you not entertained!!!!

--- AdruinoGUI/Properties/Resources.Designer.cs
+++ AdruinoGUI/Properties/Resources.Designer.cs
@@ -8,10 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace AdruinoGUI.Properties
-{
-
-
+namespace AdruinoGUI.Properties {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -19,52 +19,68 @@ namespace AdruinoGUI.Properties
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources
-    {
-
+    public class Resources {
+        
         private static global::System.Resources.ResourceManager resourceMan;
-
+        
         private static global::System.Globalization.CultureInfo resourceCulture;
-
+        
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resources()
-        {
+        internal Resources() {
         }
-
+        
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager
-        {
-            get
-            {
-                if ((resourceMan == null))
-                {
+        public static global::System.Resources.ResourceManager ResourceManager {
+            get {
+                if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("AdruinoGUI.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-
+        
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture
-        {
-            get
-            {
+        public static global::System.Globalization.CultureInfo Culture {
+            get {
                 return resourceCulture;
             }
-            set
-            {
+            set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to 131,134,137,140,143,146,147,147,146,143,140,137,134,131,128
+        ///124,121,118,115,112,109,108,108,109,112,115,118,121,124,128
+        ///27,53,80,107,133,160,187,347,373,400,427,453,480,507,533
+        ///.
+        /// </summary>
+        public static string data {
+            get {
+                return ResourceManager.GetString("data", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to 131,134,137,140,143,146,147,147,146,143,140,137,134,131,128
+        ///124,121,118,115,112,109,108,108,109,112,115,118,121,124,128
+        ///27,53,80,107,133,160,187,347,373,400,427,453,480,507,533
+        ///.
+        /// </summary>
+        public static string data1 {
+            get {
+                return ResourceManager.GetString("data1", resourceCulture);
             }
         }
     }
